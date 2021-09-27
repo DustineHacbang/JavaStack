53fd9d754cd9713612b1d6a1eb8bbbe00194749c
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-09-27 11:56:08 -0700

    Java d1

---
+++ assignment/First_java/First_java.java
@@ -0,0 +1,8 @@
+package First_java;
+public class First_java {
+    public static void main(String[] args) {
+        System.out.println(
+            "My name is Dustin Hacbang I am 23 years old My hometown is Fresno, CA"
+        );
+    }
+}
