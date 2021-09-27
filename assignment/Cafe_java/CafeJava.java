da896e58ed3c443c3e6f50fa06f0a55d776edda4
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-09-27 12:38:35 -0700

    d1

--- assignment/Cafe_java/CafeJava.java
+++ assignment/Cafe_java/CafeJava.java
@@ -24,15 +24,36 @@ public class CafeJava {
         // Order completions (add yours below)
         boolean isReadyOrder1 = false;
         boolean isReadyOrder2 = true;
+        boolean isReadyOrder3 = false;
+        boolean isReadyOrder4 = true;
 
         // APP INTERACTION SIMULATION (Add your code for the challenges below)
 
-        System.out.println(generalGreeting + customer1 + drink1 + isReadyOrder1); 
-        System.out.println(generalGreeting + customer4 + drink1 + isReadyOrder2); 
-        System.out.println(generalGreeting + customer2 + drink1 + isReadyOrder1); 
+        System.out.println(customer1 + pendingMessage);
 
-        // Example:
-        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
-    	// ** Your customer interaction print statements will go here ** //
+        // 2.) Noah ordered a cappucino
+        //     Check the status of his order and let him know. If it is ready, also tell him his total.
+        if (isReadyOrder4) {
+            System.out.println(customer4 + readyMessage);
+            System.out.println(displayTotalMessage + drink3Price);
+        }
+        else {
+            System.out.println(customer4 + pendingMessage);
+        }
+        
+        // 3.) Sam ordered 2 lattes, and would like to know the total up front. 
+        //      Also tell them their order status.
+        System.out.println(displayTotalMessage + drink2Price * 2);
+        if (isReadyOrder2) {
+            System.out.println(customer2 + readyMessage);
+        }
+        else {
+            System.out.println(customer2 + pendingMessage);
+        }
+
+
+        // 4.) Jimmy just realized he was charged for a coffee but had ordered a latte.
+        //     Tell him his new total to make up the difference.
+        System.out.println(displayTotalMessage + (drink2Price - drink1Price));
     }
 }
